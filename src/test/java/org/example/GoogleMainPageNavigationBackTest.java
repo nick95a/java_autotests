f21f308f0f10bb445186e9fa993cc9e2434fe7c5
nick95a <nick95a@yandex.ru>
2023-06-03 16:54:24 +0500

    Some autotests

---
+++ src/test/java/org/example/GoogleMainPageNavigationBackTest.java
@@ -0,0 +1,53 @@
+package org.example;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class GoogleMainPageNavigationBackTest {
+
+    // Test for navigation from google main page
+    public static WebDriver driver;
+    public static GoogleMainPage mainPage;
+    public static String url = "https://google.com";
+    public static String checkUrl = "https://www.sports.ru";
+    public static String chromeDriverPath = "/Users/nikolaimaltcev/Downloads/chromedriver";
+    public static String searchText = "Hello World";
+
+    @BeforeClass
+    public static void setup() {
+
+        System.setProperty("webdriver.chrome.driver", chromeDriverPath);
+        driver = new ChromeDriver();
+        mainPage = new GoogleMainPage(driver);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get(url);
+    }
+
+    @Test
+    public void clickTest() {
+
+        mainPage.clickSearchField();
+        mainPage.inputSearchField(searchText);
+        mainPage.clickSearchButton();
+        driver.get(url);
+        String currentUrl = mainPage.getUrl();
+        try {
+            Assert.assertNotEquals(url, currentUrl);
+            System.out.println("Test passed: Back at home page");
+        }
+        catch(Exception except) {
+            System.out.println("Test failed: Not at home page");
+        }
+    }
+
+    @AfterClass
+    public static void closeDriver() {
+        driver.quit();
+    }
+}
